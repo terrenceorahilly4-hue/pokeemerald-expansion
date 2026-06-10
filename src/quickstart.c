a4f9ad33947b51a32dd39e23ac12da34764d11e6
Terence <terrenceorahilly4@email.com>
2026-06-10 18:37:34 +0100

    Advance intro state to permanently hide trucks

--- src/quickstart.c
+++ src/quickstart.c
@@ -99,8 +99,11 @@ static void CB2_SkipToNewGame(void)
         ResetTasks();
         FlagSet(FLAG_HIDE_LITTLEROOT_TOWN_BRENDANS_HOUSE_TRUCK);
         FlagSet(FLAG_HIDE_LITTLEROOT_TOWN_MAYS_HOUSE_TRUCK);
+        VarSet(VAR_LITTLEROOT_INTRO_STATE, 3);
+        VarSet(VAR_LITTLEROOT_HOUSES_STATE_BRENDAN, 1);
+        VarSet(VAR_LITTLEROOT_HOUSES_STATE_MAY, 1);
 
-    SetWarpDestination(MAP_GROUP(MAP_FLASHBACK_ROOM), MAP_NUM(MAP_FLASHBACK_ROOM), 0, -1, -1);
+        SetWarpDestination(MAP_GROUP(MAP_FLASHBACK_ROOM), MAP_NUM(MAP_FLASHBACK_ROOM), 0, -1, -1);
         SetMainCallback2(CB2_NewGame);
     }
 }
