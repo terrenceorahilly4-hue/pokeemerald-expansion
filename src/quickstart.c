d5b5ec9d08765302eea1038c2ee01ddd8f0a5853
Terence <terrenceorahilly4@gmail.com>
2026-06-11 02:49:53 +0100

    fix: safe warp to FlashbackRoom, add required includes

--- src/quickstart.c
+++ src/quickstart.c
@@ -1,3 +1,5 @@
+#include "event_data.h"
+#include "constants/maps.h"
 #include "global.h"
 #include "config/general.h"
 #include "constants/global.h"
@@ -130,7 +132,7 @@ void Quickstart(void)
         FlagSet(FLAG_SYS_POKEMON_GET);
         FlagSet(FLAG_SYS_POKEDEX_GET);
         FlagSet(FLAG_ADVENTURE_STARTED);
-        SetWarpDestination(MAP_GROUP(MAP_BOND_OF_EIRE_BRENDANS_HOUSE_2F), MAP_NUM(MAP_FLASHBACK_ROOM), 0, -1, -1);
+        SetWarpDestination(MAP_GROUP(MAP_FLASHBACK_ROOM), MAP_NUM(MAP_FLASHBACK_ROOM), 0, -1, -1);
         SetMainCallback2(CB2_NewGame);
     }
 }
