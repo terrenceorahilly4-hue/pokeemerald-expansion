058afb3832187432c7fc710ba45c76134951e281
Terence <terrenceorahilly4@gmail.com>
2026-06-13 16:41:15 +0100

    fix: restore missing macro and constant includes to event_scripts.s

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -1,3 +1,8 @@
+.include "asm/macros.inc"
+.include "constants/constants.inc"
+
+.section script_data, "aw", %progbits
+
 .include "data/scripts/field_move_scripts.inc"
 .include "data/scripts/item_ball_scripts.inc"
 .include "data/scripts/vanilla_stubs.inc"
