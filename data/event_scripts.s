d9f5ba04a873b1107ffedec74847fc875938f1c1
Terence <terrenceorahilly4@email.com>
2026-06-14 00:09:23 +0100

    fix: include BOND_Cliff map script in event_scripts.s

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -1482,3 +1482,4 @@ EventScript_PalletTown_PlayersHouse_2F_TurnOnPC::
 .include "data/maps/MAP_FLASHBACK_ROOM/scripts.inc"
 .include "data/scripts/custom_map_scripts.inc"
 .include "data/maps/BOND_BirchsLab/scripts.inc"
+.include "data/maps/BOND_Cliff/scripts.inc"
