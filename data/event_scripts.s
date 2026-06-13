8e9475de8df3c51a66dd671bf0a1cd7b00581251
Terence <terrenceorahilly4@gmail.com>
2026-06-13 14:31:25 +0100

    fix: restore registry and register BOND_Cliff

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -1488,3 +1488,4 @@ EventScript_PalletTown_PlayersHouse_2F_TurnOnPC::
 .include "data/maps/BOND_NeighboursHouse_2F/scripts.inc"
 .include "data/maps/BOND_AnTineBheo/scripts.inc"
 .include "data/maps/BOND_Cliff/scripts.inc"
+.include "data/maps/BOND_Cliff/scripts.inc"
