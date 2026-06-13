028eccf8f95f035ec24bbdadcfe04931dd845ebf
Terence <terrenceorahilly4@email.com>
2026-06-13 21:10:45 +0100

    build: deploy core namespace architecture to cloud runner

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -1,9 +1,9 @@
 .include "asm/macros.inc"
 .include "constants/constants.inc"
 
+.section script_data, "aw", %progbits
+
 .include "data/scripts/field_move_scripts.inc"
 .include "data/scripts/item_ball_scripts.inc"
 .include "data/scripts/vanilla_stubs.inc"
-.include "data/scripts/custom_map_scripts.inc"
-
-.section script_data, "aw", %progbits
+.include "data/maps/scripts.inc"
